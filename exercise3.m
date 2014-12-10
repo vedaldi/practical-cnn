185eddf54533168c704ef6edae761e73b1715abd
Andrea Vedaldi <vedaldi@gmail.com>
2014-12-10 19:21:57 +0000

    exercise3: beta

--- exercise3.m
+++ exercise3.m
@@ -4,97 +4,133 @@
 
 setup ;
 
-% Load an image and compute its edge map
-im_ = rgb2gray(im2single(imread('data/dots.jpg'))) ;
-im = [im_, im_ ; flipud(im_), flipud(im_)'] ;
-im = im_ ;
-[features,background] = extractImageBlobs(im) ;
-b = 1 ;
-features = features(1+b:end-b,1+b:end-b) ;
-background = background(1+b:end-b,1+b:end-b) ;
+% -------------------------------------------------------------------------
+% Part 3.1: Load an example image and generate its labels
+% -------------------------------------------------------------------------
 
-im = vl_imsmooth(im,3) ;
-im = im - mean(im(:)) ;
+% Load an image
+im = rgb2gray(im2single(imread('data/dots.jpg'))) ;
+
+% Compute the location of black blobs in the image
+[pos,neg] = extractBlackBlobs(im) ;
 
 figure(1) ; clf ; 
-subplot(1,3,1) ; imagesc(im) ; axis equal ;
-subplot(1,3,2) ; imagesc(features) ; axis equal ;
-subplot(1,3,3) ; imagesc(background) ; axis equal ;
+subplot(1,3,1) ; imagesc(im) ; axis equal ; title('image') ;
+subplot(1,3,2) ; imagesc(pos) ; axis equal ; title('positive points') ;
+subplot(1,3,3) ; imagesc(neg) ; axis equal ; title('negative points') ;
 colormap gray ; 
 
-% Initialize the network
-w = randn(3, 3, 1) ;
-%w = w * 10 ;
-if 0
-w =[...
- 0  1 0 ;
- 1 -4 1 ;
-0  1 0 ] * 100;
-end
+% -------------------------------------------------------------------------
+% Part 3.2: Image preprocessing
+% -------------------------------------------------------------------------
+
+% Pre-smooth the image
+im = vl_imsmooth(im,3) ;
+
+% Subtract median value
+im = im - median(im(:)) ;
+
+% -------------------------------------------------------------------------
+% Part 3.3: Learning with stochastic gradient descent
+% -------------------------------------------------------------------------
+
+
+% SGD parameters:
+% - numIterations: maximum number of iterations
+% - rate: learning rate
+% - momentum: momentum rate
+% - shrinkRate: shrinkage rate (or coefficient of the L2 regulariser)
+% - plotPeriod: how often to plot
+
+numIterations = 500 ;
+rate = 5 ;
+momentum = 0.9 ;
+energy = zeros(1, numIterations) ;
+shrinkRate = 0.0001 ;
+plotPeriod = 10 ;
+
+% Initial CNN parameters:
+w = 10 * randn(3, 3, 1) ;
 w = single(w - mean(w(:))) ;
 b = single(0) ;
-dzdw_momentum = zeros('like', w) ;
-dzdb_momentum = zeros('like', b) ;
 
-% SGD parameters
-T = 500 ;
-eta = 5 ;
-momentum = 0.9 ;
-energy = zeros(1, T) ;
-shrink = 0.000005 ;
-wn = 2 ;
-y = zeros(size(features),'single') ;
-y(features) = +1 ;
-y(background) = -1 ;
-pos = features ;
-neg = background ;
-ignore = y(:) == 0 ;
+% Create pixel-level labes to compute the loss
+y = zeros(size(pos),'single') ;
+y(pos) = +1 ;
+y(neg) = -1 ;
+
+% Initial momentum
+w_momentum = zeros('like', w) ;
+b_momentum = zeros('like', b) ;
 
 % SGD with momentum
-for t = 1:T
+for t = 1:numIterations
   
-  if t > 100, eta=1;end
   % Forward pass
-  res = edgecnn(im, w, b) ;
+  res = tinycnn(im, w, b) ;
   
   % Loss  
-  z = y .* (res.x4 - 1) ;
-  E(t) = ...
-    mean(max(0, 1 - res.x4(pos))) + ...
-    wn*mean(max(0, res.x4(neg))) ;
-  dzdx4 = ...
-    - single(res.x4 < 1 & pos) / sum(pos(:)) + ...
-    + wn*single(res.x4 > 0 & neg) / sum(neg(:)) ; 
+  z = y .* (res.x3 - 1) ;
+
+  E(1,t) = ...
+    mean(max(0, 1 - res.x3(pos))) + ...
+    mean(max(0, res.x3(neg))) ;
+  E(2,t) = 0.5 * shrinkRate * sum(w(:).^2) ;
+  E(3,t) = E(1,t) + E(2,t) ;
+
+  dzdx3 = ...
+    - single(res.x3 < 1 & pos) / sum(pos(:)) + ...
+    + single(res.x3 > 0 & neg) / sum(neg(:)) ; 
 
   % Backward pass
-  res = edgecnn(im, w, b, dzdx4) ;
+  res = tinycnn(im, w, b, dzdx3) ;
   
+  % Update momentum
+  w_momentum = momentum * w_momentum + rate * (res.dzdw + shrinkRate * w) ;
+  b_momentum = momentum * b_momentum + rate * 0.1 * res.dzdb ; 
+
   % Gradient step
-  dzdw_momentum = momentum * dzdw_momentum + res.dzdw ;  
-  dzdb_momentum = momentum * dzdb_momentum + res.dzdb ; 
-  w = (1 - shrink) * w - eta * dzdw_momentum ;
-  b = b - 0.1 * eta * dzdb_momentum ;
-  
-  w(:)'
-  b
-  
+  w = w - w_momentum ;
+  b = b - b_momentum ;
+
   % Plots
-  if mod(t-1, 1) == 0
-    fp = res.x4 > 0 & y < 0 ;
-    fn = res.x4 < 1 & y > 0 ;
-    tn = res.x4 <= 0 & y < 0 ;
-    tp = res.x4 >= 1 & y > 0 ;
+  if mod(t-1, plotPeriod) == 0 || t == numIterations
+    fp = res.x3 > 0 & y < 0 ;
+    fn = res.x3 < 1 & y > 0 ;
+    tn = res.x3 <= 0 & y < 0 ;
+    tp = res.x3 >= 1 & y > 0 ;
     err = cat(3, fp|fn , tp|tn, y==0) ;
+       
+    figure(2) ; clf ;
+    colormap gray ;
     
-    figure(2) ; clf ; colormap gray ;
-    subplot(2,3,1) ; imagesc(res.x4) ; axis equal ; title('network output') ;
-    subplot(2,3,2) ; plot(1:t, E(1:t)) ; grid on ; title('objective') ;
-    subplot(2,3,3) ; vl_imarraysc(w) ; title('filter slices') ; axis equal ;
-    subplot(2,3,4) ; imagesc(res.x2) ; title('first layer output') ; axis equal ;
-    subplot(2,3,5) ; image(err) ;
+    subplot(2,3,1) ;
+    plot(1:t, E(:,1:t)') ;
+    grid on ; title('objective') ;
+    ylim([0 1.5]) ; legend('error', 'regularizer', 'total') ;
+        
+    subplot(2,3,2) ; hold on ;
+    [h,x]=hist(res.x3(pos(:)),30) ; plot(x,h/max(h),'g') ;
+    [h,x]=hist(res.x3(neg(:)),30) ; plot(x,h/max(h),'r') ;
+    plot([0 0], [0 1], 'b--') ;
+    plot([1 1], [0 1], 'b--') ;
+    xlim([-2 3]) ;
+    title('histograms of scores') ; legend('pos', 'neg') ;
+    
+    subplot(2,3,3) ;
+    vl_imarraysc(w) ;
+    title('learned filter') ; axis equal ;
+    
+    subplot(2,3,4) ;
+    imagesc(res.x3) ;
+    title('network output') ; axis equal ; 
+
+    subplot(2,3,5) ;
+    imagesc(res.x2) ;
+    title('first layer output') ; axis equal ;
+
     subplot(2,3,6) ;
-    [h,x]=hist(res.x4(pos(:)),30) ;plot(x,h/max(h),'g') ;
-    hold on ;
-    [h,x]=hist(res.x2(neg(:)),30) ;plot(x,h/max(h),'r') ;
+    image(err) ;    
+    title('red: pred. error, green: correct, blue: ignore') ;
   end
 end

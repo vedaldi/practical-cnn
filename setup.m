54c807d995d0ed1c152eefa1b589f669a8324429
Andrea Vedaldi <vedaldi@gmail.com>
2017-11-27 17:26:30 +0100

    take out Imreadjpeg for real

--- setup.m
+++ setup.m
@@ -12,14 +12,18 @@ try
   vl_nnconv(single(1),single(1),[]) ;
 catch
   warning('VL_NNCONV() does not seem to be compiled. Trying to compile it now.') ;
-  vl_compilenn('enableGpu', opts.useGpu, 'verbose', opts.verbose) ;
+  vl_compilenn('enableGpu', opts.useGpu, ...
+               'enableImReadJPEG', opts.enableImReadJPEG, ...
+               'verbose', opts.verbose) ;
 end
 
 if opts.useGpu
   try
     vl_nnconv(gpuArray(single(1)),gpuArray(single(1)),[]) ;
   catch
-    vl_compilenn('enableGpu', opts.useGpu, 'verbose', opts.verbose) ;
+    vl_compilenn('enableGpu', opts.useGpu, ...
+                 'enableImReadJPEG', opts.enableImReadJPEG, ...
+                 'verbose', opts.verbose) ;
     warning('GPU support does not seem to be compiled in MatConvNet. Trying to compile it now') ;
   end
 end
